6f3ba8db2c7068b1c9366ae622a6a42c37928a2d
David Malchin <malchin459@gmail.com>
2024-03-29 00:51:34 +0200

    Ignore multiple `/` in request url

--- src/http/url.ts
+++ src/http/url.ts
@@ -1,5 +1,5 @@
 import { IncomingMessage } from 'node:http';
 
 export function getRequestUrl(request: IncomingMessage) {
-    return new URL(request.url!, `http://${request.headers.host}`);
+    return new URL(request.url!.replaceAll(/\/+/g, '/').replace(/\/$/, ''), `http://${request.headers.host}`);
 }
