74d06d73a7260d73ba4da25fddad41d283e6d987
David Malchin <malchin459@gmail.com>
2024-03-31 17:26:50 +0300

    Move creating posts inside categories route

--- src/api/input/post-create.ts
+++ src/api/input/post-create.ts
@@ -4,7 +4,6 @@ import {
 } from './message-create.js';
 
 export type PostCreateObject = {
-    readonly category: number;
     readonly message: MessageCreateObject;
     readonly title: string;
 };
@@ -13,8 +12,6 @@ export function isPostCreateObject(obj: unknown): obj is PostCreateObject {
     return (
         obj !== null &&
         typeof obj === 'object' &&
-        'category' in obj &&
-        typeof obj.category === 'number' &&
         'message' in obj &&
         isMessageCreateObject(obj.message) &&
         'title' in obj &&
--- src/routes/categories/_categoryId/posts/POST.ts
+++ src/routes/categories/_categoryId/posts/POST.ts
@@ -1,17 +1,20 @@
-import { ApiError } from '../../api/enums/error.js';
-import { isPostCreateObject } from '../../api/input/post-create.js';
-import { createPostObject } from '../../api/objects/post.js';
-import { validatePostTitle } from '../../api/validators/post-title.js';
-import { prisma } from '../../env.js';
-import { handleAuthorization } from '../../http/handlers/authorization.js';
-import { RouteHandler } from '../../http/handlers/index.js';
-import { handleJson } from '../../http/handlers/json.js';
-import { writeErrorReply } from '../../http/replies/error.js';
-import { writeJsonReply } from '../../http/replies/json.js';
-import { HttpStatusCode } from '../../http/status.js';
+import { ApiError } from '../../../../api/enums/error.js';
+import { isPostCreateObject } from '../../../../api/input/post-create.js';
+import { createPostObject } from '../../../../api/objects/post.js';
+import { validatePostTitle } from '../../../../api/validators/post-title.js';
+import { prisma } from '../../../../env.js';
+import { handleAuthorization } from '../../../../http/handlers/authorization.js';
+import { RouteHandler } from '../../../../http/handlers/index.js';
+import { handleJson } from '../../../../http/handlers/json.js';
+import { writeErrorReply } from '../../../../http/replies/error.js';
+import { writeJsonReply } from '../../../../http/replies/json.js';
+import { HttpStatusCode } from '../../../../http/status.js';
 
 export default (async (props) => {
-    const { response } = props;
+    const {
+        response,
+        params: [rawId],
+    } = props;
 
     const user = await handleAuthorization(props);
     if (!user) return;
@@ -23,9 +26,17 @@ export default (async (props) => {
         return writeErrorReply(response, ApiError.InvalidObject);
     }
 
+    const categoryId = parseInt(rawId!);
+    if (isNaN(categoryId)) {
+        return writeErrorReply(response, ApiError.UnknownCategory);
+    }
+
     const category = await prisma.category.findFirst({
-        where: { id: data.category },
+        where: {
+            id: categoryId,
+        },
     });
+
     if (!category) {
         return writeErrorReply(response, ApiError.UnknownCategory);
     }
@@ -45,9 +56,7 @@ export default (async (props) => {
             },
             title: data.title,
             category: {
-                connect: {
-                    id: data.category,
-                },
+                connect: category,
             },
         },
     });
