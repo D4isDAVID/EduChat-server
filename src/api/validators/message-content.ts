12615b1722b1dfd9cde8e6e87506f9d4a05572fe
David Malchin <malchin459@gmail.com>
2024-05-19 10:05:06 +0300

    Fix returned error when validating message content

--- src/api/validators/message-content.ts
+++ src/api/validators/message-content.ts
@@ -2,6 +2,6 @@ import { ApiError } from '../enums/error.js';
 
 export function validateMessageContent(content: string): ApiError | undefined {
     if (content.length < 1 || content.length > 2000) {
-        return ApiError.BadPostTitleLength;
+        return ApiError.BadMessageContentLength;
     }
 }
