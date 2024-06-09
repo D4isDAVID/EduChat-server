2eeb32504645df965150ba20a2424197ddc0602b
David Malchin <malchin459@gmail.com>
2024-06-09 12:58:16 +0300

    Mark related notifications when getting own user

--- src/routes/api/users/@me/GET.ts
+++ src/routes/api/users/@me/GET.ts
@@ -1,4 +1,6 @@
+import { NotificationType } from '../../../../api/enums/notification-type.js';
 import { createUserObject } from '../../../../api/objects/user.js';
+import { prisma } from '../../../../env.js';
 import { handleAuthorization } from '../../../../http/handlers/authorization.js';
 import { RouteHandler } from '../../../../http/handlers/index.js';
 import { writeJsonReply } from '../../../../http/replies/json.js';
@@ -8,6 +10,19 @@ export default (async (props) => {
 
     const user = await handleAuthorization(props);
     if (!user) return;
+
+    await prisma.notification.updateMany({
+        where: {
+            targetId: user.id,
+            OR: [
+                { type: NotificationType.HelperGranted },
+                { type: NotificationType.HelperRevoked },
+                { type: NotificationType.AdminGranted },
+                { type: NotificationType.AdminRevoked },
+            ],
+        },
+        data: { read: true },
+    });
 
     return writeJsonReply(response, await createUserObject(user));
 }) satisfies RouteHandler;
